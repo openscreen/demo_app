4ef6313e12e03b94cb4afdcb2ace16af2b9669ef
UrbanScreen <rodrigo.arbolurbano@gmail.com>
2012-02-26 13:15:24 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '171c2fb4ee818b8c7c7a5e5755b1b6fbee4903dbeafbb273c1d43f4a18856e9b87b6d708d1165c29455af8c1d9c5d45d8b79936413c6df8069e0061c0d9db5b4'
